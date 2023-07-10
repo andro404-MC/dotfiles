23e90c5edf2480e477acf90e5c4492817edb261b
Untemi <94703538+andro404-MC@users.noreply.github.com>
2023-07-10 11:27:56 +0100

    fix mpv + new rofi applet

--- .config/rofi/powermenu/type-2/powermenu.sh
+++ .config/rofi/powermenu/type-2/powermenu.sh
@@ -97,7 +97,7 @@ case ${chosen} in
         ;;
     $lock)
 		if [[ -x '/usr/bin/betterlockscreen' ]]; then
-			betterlockscreen -l blur 0.8
+			betterlockscreen -l blur
 		elif [[ -x '/usr/bin/i3lock' ]]; then
 			i3lock
 		fi
