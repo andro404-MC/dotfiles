29098644d4f549660b6f7ff4fff8a7fb243b62a9
Untemi <94703538+andro404-MC@users.noreply.github.com>
2023-09-26 10:43:15 +0100

    we live we love we lie

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,7 +9,7 @@
 
 function fish_greeting
   echo ""
-  cfonts unos -a left -f block -g "#f7768e","#7aa2f7" --transition-gradient -s
+  cfonts "#!" -a left -f block -g "#f7768e","#7aa2f7" --transition-gradient -s
 end
 function keyb
   xev | awk -F'[ )]+' '/^KeyPress/ { a[NR+2] } NR in a { printf "%-3s %s\n", $5, $8 }'
--- scripts/layoutswitch.sh
+++
@@ -1,8 +0,0 @@
-current_layout=$(setxkbmap -query | grep layout | awk '{print $2}')
-
-if [ "$current_layout" = "fr" ]; then
-    setxkbmap ara
-else
-    setxkbmap fr
-fi
-
